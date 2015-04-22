b54222b7e21400148adc8f0586ac2a6189625744
Anton Andersson (C More) <anton.andersson@c02m62dsf6t5.tv4.se>
2015-04-22 11:04:04 +0200

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Anton Andersson (C More) on 2015-04-22.
+//  Copyright (c) 2015 iamanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
